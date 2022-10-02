cdb0ee40501fddf1c8baeb9151fd6a5e00310083
Maru <t108360758@gmail.org.tw>
2022-10-02 14:59:42 +0800

    button OK

--- space-invaders/src/main.ts
+++ space-invaders/src/main.ts
@@ -1,23 +1,96 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { Application, Graphics,Sprite,Texture } from "pixi.js"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//test button excode  https://pixijs.io/examples/#/interaction/interactivity.js
+const app = new Application();
+
+document.querySelector("#app")?.append(app.view);
+
+// create a background...
+const background = Sprite.from("/picture/background.jpg");
+background.width = app.screen.width;
+background.height = app.screen.height;
+
+
+app.stage.addChild(background);
+
+const soundbuttonoff = Texture.from("./picture/mute_rb.png");
+const soundbuttonon = Texture.from("./picture/sound_rb.png");
+const buttons = [];
+const buttonPositions = [
+  175, 75,
+];
+
+
+  const button = new Sprite(soundbuttonoff);
+  button.width=200;
+  button.height=200;
+  button.anchor.set(0.5);
+  button.x = buttonPositions[0];
+  button.y = buttonPositions[1];
+  // make the button interactive...
+  button.interactive = true;
+  button.buttonMode = true;
+
+  button
+  // Mouse & touch events are normalized into
+  // the pointer* events for handling different
+  // button events.
+      .on('pointerdown', onButtonDown)
+      .on('pointerup', onButtonUp)
+      .on('pointerupoutside', onButtonUp)
+      .on('pointerover', onButtonOver)
+      .on('pointerout', onButtonOut);
+  // Use mouse-only events
+  // .on('mousedown', onButtonDown)
+  // .on('mouseup', onButtonUp)
+  // .on('mouseupoutside', onButtonUp)
+  // .on('mouseover', onButtonOver)
+  // .on('mouseout', onButtonOut)
+
+  // Use touch-only events
+  // .on('touchstart', onButtonDown)
+  // .on('touchend', onButtonUp)
+  // .on('touchendoutside', onButtonUp)
+
+  // add it to the stage
+  app.stage.addChild(button);
+
+  // add button to array
+  buttons.push(button);
+
+
+// set some silly values...
+buttons[0].scale.set(1.2);
+buttons[2].rotation = Math.PI / 10;
+buttons[3].scale.set(0.8);
+buttons[4].scale.set(0.8, 1.2);
+buttons[4].rotation = Math.PI;
+
+function onButtonDown(this: any) {
+  this.isdown = true;
+  this.texture = soundbuttonoff;
+  this.alpha = 1;
+}
+
+function onButtonUp(this: any) {
+  this.isdown = false;
+      this.texture = soundbuttonoff;
+}
+
+function onButtonOver(this: any) {
+  this.isOver = true;
+  if (this.isdown) {
+      return;
+  }
+  this.texture = soundbuttonon;
+}
+
+function onButtonOut(this: any) {
+  this.isOver = false;
+  if (this.isdown) {
+      return;
+  }
+  this.texture = soundbuttonon;
+}
